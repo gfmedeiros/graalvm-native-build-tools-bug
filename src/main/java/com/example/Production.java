49d2389a40c665f56a89a4440e63cb4900e050d5
Gabriel Felipe Medeiros <gabriel.felipe@externo.cresol.coop.br>
2022-08-15 20:10:53 -0300

    Remove PicoCLI to keep example minimal

--- src/main/java/com/example/Production.java
+++ src/main/java/com/example/Production.java
@@ -1,12 +1,8 @@
 package com.example;
 
-import com.example.cli.RootCommand;
-import picocli.CommandLine;
-
 public class Production {
 
 	public static void main(String[] args) {
-		var status =  new CommandLine(new RootCommand()).execute(args);
-		System.exit(status);
+		System.out.println("Hello, world!");
 	}
 }
